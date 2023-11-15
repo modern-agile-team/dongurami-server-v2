cf41a2dd5835ab18b104d5e379b3b225e030478b
hobiJeong <jjb26433@naver.com>
2023-11-15 22:06:32 +0900

    modify(#1): add .env in .gitignore

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
-  app.enableCors({
-    origin: true, // 또는 특정 도메인을 설정
-    methods: 'GET ,HEAD, PUT, PATCH, POST, DELETE',
-    credentials: true, // 이 옵션을 true로 설정하여 쿠키 전송을 허용
-  });
+  app.enableCors();
   const port = process.env.PORT || 8080;
   app.useLogger(logger);
   await app.listen(port);

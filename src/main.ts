2fd2e38852c2b4ddb6d921ff4dd70460aded9f3d
hobiJeong <jjb26433@naver.com>
2023-11-17 13:38:50 +0900

    setting/#1: apply configService , http excpetion filter(Global)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   const logger = new Logger();
   app.enableCors();
   const port = process.env.PORT || 8080;
   app.useLogger(logger);
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,

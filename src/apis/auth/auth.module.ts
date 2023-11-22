e9e83e7eaf4e840330b99991c42994657d6a7189
LeeSeokHo <rrgks@naver.com>
2023-11-22 21:46:51 +0900

    feat: forDevelopmentMiddleware

--- src/apis/auth/auth.module.ts
+++ src/apis/auth/auth.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModuleOptionsFactory } from '@src/apis/auth/jwt/jwt-module-options.factory';
 import { JwtStrategy } from '@src/apis/auth/jwt/jwt.strategy';
 import { UsersModule } from '@src/apis/users/users.module';
+import { UseDevelopmentMiddleware } from '@src/middlewares/use-development.middleware';
 import { AuthController } from './contollers/auth.controller';
 import { AuthService } from './services/auth.service';
 
@@ -18,4 +24,11 @@ import { AuthService } from './services/auth.service';
   controllers: [AuthController],
   providers: [AuthService, JwtModuleOptionsFactory, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UseDevelopmentMiddleware).forRoutes({
+      path: 'auth/access-token/:userId',
+      method: RequestMethod.GET,
+    });
+  }
+}

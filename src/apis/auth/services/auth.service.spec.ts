6cd570ef256f0bc0571bbba664183de3d1dc70bf
LeeSeokHo <rrgks@naver.com>
2023-11-22 21:40:52 +0900

    test: auth

--- src/apis/auth/services/auth.service.spec.ts
+++ src/apis/auth/services/auth.service.spec.ts
@@ -1,18 +1,57 @@
+import { faker } from '@faker-js/faker';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Payload } from '@src/apis/auth/type/auth.type';
+import { AppConfigService } from '@src/core/app-config/services/app-config.service';
+import { mockAppConfigService, mockJwtService } from '@test/mock/mock.service';
 import { AuthService } from './auth.service';
 
-describe('AuthService', () => {
+describe(AuthService.name, () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: AppConfigService,
+          useValue: mockAppConfigService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe(AuthService.prototype.generateToken.name, () => {
+    let payload: Payload;
+
+    beforeEach(() => {
+      payload = {
+        id: NaN,
+      };
+    });
+
+    it('generateAccessToken', () => {
+      const token = 'token';
+
+      payload.id = faker.number.int();
+
+      mockJwtService.sign.mockReturnValue(token);
+
+      expect(service.generateToken(payload)).toBe(token);
+    });
   });
 });
--- test/mock/mock.service.ts
+++ test/mock/mock.service.ts
@@ -1,9 +1,34 @@
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from '@src/apis/auth/services/auth.service';
 import { UsersService } from '@src/apis/users/services/users.service';
+import { AppConfigService } from '@src/core/app-config/services/app-config.service';
 import { EncryptionService } from '@src/libs/encryption/services/encryption.service';
 import { MockProvider } from '@test/mock/mock.type';
 
 export const mockEncryptionService: MockProvider<EncryptionService> = {
   hash: jest.fn(),
+};
+
+export const mockJwtService: MockProvider<JwtService> = {
+  sign: jest.fn(),
+  signAsync: jest.fn(),
+  verify: jest.fn(),
+  verifyAsync: jest.fn(),
+  decode: jest.fn(),
+};
+
+export const mockAppConfigService: MockProvider<AppConfigService> = {
+  get: jest.fn(),
+  getList: jest.fn(),
+  getAll: jest.fn(),
+  getAllMap: jest.fn(),
+  isLocal: jest.fn(),
+  isDevelopment: jest.fn(),
+  isProduction: jest.fn(),
+};
+
+export const mockAuthService: MockProvider<AuthService> = {
+  generateToken: jest.fn(),
 };
 
 export const mockUsersService: MockProvider<UsersService> = {

eeb90c7d9e13cc9520cf0fd42960d7d5fc95de22
LeeSeokHo <rrgks@naver.com>
2023-11-22 20:16:23 +0900

    test: usersController.create

--- src/apis/users/controllers/users.controller.spec.ts
+++ src/apis/users/controllers/users.controller.spec.ts
@@ -1,12 +1,23 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserRequestBodyDto } from '@src/apis/users/dto/create-user-request-body.dto';
+import { UserDto } from '@src/apis/users/dto/user.dto';
+import { UsersService } from '@src/apis/users/services/users.service';
+import { mockUsersService } from '@test/mock/mock.service';
 import { UsersController } from './users.controller';
 
-describe('UsersController', () => {
+describe(UsersController.name, () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +25,25 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe(UsersController.prototype.create.name, () => {
+    let createUserRequestBodyDto: CreateUserRequestBodyDto;
+    let newUser: UserDto;
+
+    beforeEach(() => {
+      createUserRequestBodyDto = new CreateUserRequestBodyDto();
+      newUser = new UserDto();
+    });
+
+    it('create user', async () => {
+      newUser.id = faker.number.int();
+
+      mockUsersService.create.mockResolvedValue(newUser);
+
+      await expect(
+        controller.create(createUserRequestBodyDto),
+      ).resolves.toEqual({ user: newUser });
+    });
   });
 });

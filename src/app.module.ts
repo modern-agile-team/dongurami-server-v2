7887fd37d745a732b41852292ec4933d758c836d
LeeSeokHo <rrgks@naver.com>
2023-11-25 18:03:59 +0900

    delete: AppController, AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from '@src/apis/api.module';
-import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
 import { CoreModule } from '@src/core/core.module';
 import { LibsModule } from '@src/libs/libs.module';
 
 @Module({
   imports: [ApiModule, CoreModule, LibsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

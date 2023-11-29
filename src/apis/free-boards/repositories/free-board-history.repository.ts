07093a90985118e74455bad0bd28d995066d666a
LeeSeokHo <rrgks@naver.com>
2023-11-29 15:54:37 +0900

    feat: free board respotiroy

--- src/apis/free-boards/repositories/free-board-history.repository.ts
+++ src/apis/free-boards/repositories/free-board-history.repository.ts
@@ -1,1 +1,8 @@
-export class FreeBoardRepository {}
+import { FreeBoardHistory } from '@src/entities/FreeBoardHistory';
+import { DataSource, Repository } from 'typeorm';
+
+export class FreeBoardHistoryRepository extends Repository<FreeBoardHistory> {
+  constructor(private dataSource: DataSource) {
+    super(FreeBoardHistory, dataSource.createEntityManager());
+  }
+}

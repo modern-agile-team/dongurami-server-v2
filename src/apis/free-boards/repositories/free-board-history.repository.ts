2d3b84df59450fe4396cdcf7c16ac7f01f334f9d
LeeSeokHo <rrgks@naver.com>
2023-11-29 17:29:41 +0900

    delete: freeBoardRepository

--- src/apis/free-boards/repositories/free-board-history.repository.ts
+++
@@ -1,8 +0,0 @@
-import { FreeBoardHistory } from '@src/entities/FreeBoardHistory';
-import { DataSource, Repository } from 'typeorm';
-
-export class FreeBoardHistoryRepository extends Repository<FreeBoardHistory> {
-  constructor(private dataSource: DataSource) {
-    super(FreeBoardHistory, dataSource.createEntityManager());
-  }
-}
--- src/apis/free-boards/repositories/free-board.repository.ts
+++
@@ -1,8 +0,0 @@
-import { FreeBoard } from '@src/entities/FreeBoard';
-import { DataSource, Repository } from 'typeorm';
-
-export class FreeBoardRepository extends Repository<FreeBoard> {
-  constructor(private dataSource: DataSource) {
-    super(FreeBoard, dataSource.createEntityManager());
-  }
-}

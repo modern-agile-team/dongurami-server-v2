b6abbb30ec369896a27a804cfa078fedf416cc0e
hobiJeong <jjb26433@naver.com>
2023-11-29 19:34:39 +0900

    feat/#59: add validation

--- src/apis/major/dto/create-major-request-body.dto.ts
+++ src/apis/major/dto/create-major-request-body.dto.ts
@@ -1,8 +1,12 @@
+import { IsString } from 'class-validator';
 import { MajorDto } from './major.dto';
 
 export class CreateMajorRequestBodyDto
   implements Pick<MajorDto, 'code' | 'name'>
 {
+  @IsString()
   code: string;
+
+  @IsString()
   name: string;
 }
--- src/apis/major/dto/major.dto.ts
+++ src/apis/major/dto/major.dto.ts
@@ -1,14 +1,21 @@
 import { Major } from '@src/entities/Major';
+import { IsDate, IsNumber, IsString } from 'class-validator';
 
 export class MajorDto
   implements
     Pick<Major, 'id' | 'code' | 'name' | 'memo' | 'createdAt' | 'updatedAt'>
 {
+  @IsNumber()
   id: number;
+  @IsString()
   code: string;
+  @IsString()
   name: string;
+  @IsString()
   memo: string;
+  @IsDate()
   createdAt: Date;
+  @IsDate()
   updatedAt: Date;
 
   constructor(majorDto: Partial<MajorDto> = {}) {

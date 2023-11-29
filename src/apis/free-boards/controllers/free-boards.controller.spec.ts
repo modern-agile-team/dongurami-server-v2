8ae5fa0fedd23773e492d9314cbf9ce8cf0fd5d3
LeeSeokHo <rrgks@naver.com>
2023-11-29 17:42:53 +0900

    test: freeBoardsController.create

--- src/apis/free-boards/controllers/free-boards.controller.spec.ts
+++ src/apis/free-boards/controllers/free-boards.controller.spec.ts
@@ -1,20 +1,58 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateFreeBoardDto } from '@src/apis/free-boards/dto/create-free-board.dto';
+import { FreeBoardDto } from '@src/apis/free-boards/dto/free-board.dto';
+import { UserDto } from '@src/apis/users/dto/user.dto';
+import { mockFreeBoardsService } from '@test/mock/mock.service';
 import { FreeBoardsService } from '../services/free-board.service';
 import { FreeBoardsController } from './free-boards.controller';
 
-describe('FreeBoardController', () => {
+describe(FreeBoardsController.name, () => {
   let controller: FreeBoardsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FreeBoardsController],
-      providers: [FreeBoardsService],
+      providers: [
+        {
+          provide: FreeBoardsService,
+          useValue: mockFreeBoardsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<FreeBoardsController>(FreeBoardsController);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe(FreeBoardsController.prototype.create.name, () => {
+    let user: UserDto;
+    let createFreeBoardDto: CreateFreeBoardDto;
+
+    let newFreeBoard: FreeBoardDto;
+
+    beforeEach(() => {
+      user = new UserDto();
+      createFreeBoardDto = new CreateFreeBoardDto();
+
+      newFreeBoard = new FreeBoardDto();
+    });
+
+    it('create free board', async () => {
+      user.id = faker.number.int();
+
+      mockFreeBoardsService.create.mockResolvedValue(newFreeBoard);
+
+      await expect(
+        controller.create(user, createFreeBoardDto),
+      ).resolves.toEqual(newFreeBoard);
+    });
   });
 });
--- test/mock/mock.service.ts
+++ test/mock/mock.service.ts
@@ -1,6 +1,7 @@
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from '@src/apis/auth/services/auth.service';
 import { FreeBoardHistoryService } from '@src/apis/free-boards/free-board-history/services/free-board-history.service';
+import { FreeBoardsService } from '@src/apis/free-boards/services/free-board.service';
 import { UsersService } from '@src/apis/users/services/users.service';
 import { AppConfigService } from '@src/core/app-config/services/app-config.service';
 import { EncryptionService } from '@src/libs/encryption/services/encryption.service';
@@ -38,6 +39,10 @@ export const mockUsersService: MockProvider<UsersService> = {
   create: jest.fn(),
   findOneById: jest.fn(),
   findOneBy: jest.fn(),
+};
+
+export const mockFreeBoardsService: MockProvider<FreeBoardsService> = {
+  create: jest.fn(),
 };
 
 export const mockFreeBoardHistoryService: MockProvider<FreeBoardHistoryService> =

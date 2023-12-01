099b1d6c9a5af2cdcc95f807046f342736b03070
LeeSeokHo <rrgks@naver.com>
2023-12-01 17:25:28 +0900

    test: FreeboardsController.putUpdate

--- src/apis/free-boards/controllers/free-boards.controller.spec.ts
+++ src/apis/free-boards/controllers/free-boards.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CreateFreeBoardDto } from '@src/apis/free-boards/dto/create-free-board.dto';
 import { FindFreeBoardListQueryDto } from '@src/apis/free-boards/dto/find-free-board-list-query.dto';
 import { FreeBoardDto } from '@src/apis/free-boards/dto/free-board.dto';
+import { PutUpdateFreeBoardDto } from '@src/apis/free-boards/dto/put-update-free-board.dto';
 import { UserDto } from '@src/apis/users/dto/user.dto';
 import { mockFreeBoardsService } from '@test/mock/mock.service';
 import { FreeBoardsService } from '../services/free-board.service';
@@ -70,6 +71,26 @@ describe(FreeBoardsController.name, () => {
       await expect(
         controller.findAllAndCount(findFreeBoardListQueryDto),
       ).resolves.toEqual([[], 0]);
+    });
+  });
+
+  describe(FreeBoardsController.prototype.putUpdate.name, () => {
+    let user: UserDto;
+    let freeBoardId: number;
+    let putUpdateFreeBoardDto: PutUpdateFreeBoardDto;
+
+    beforeEach(() => {
+      user = new UserDto();
+      freeBoardId = NaN;
+      putUpdateFreeBoardDto = new PutUpdateFreeBoardDto();
+    });
+
+    it('put update', async () => {
+      mockFreeBoardsService.putUpdate.mockResolvedValue(new FreeBoardDto());
+
+      await expect(
+        controller.putUpdate(user, freeBoardId, putUpdateFreeBoardDto),
+      ).resolves.toBeInstanceOf(FreeBoardDto);
     });
   });
 });

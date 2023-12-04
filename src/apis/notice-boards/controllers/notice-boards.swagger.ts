dff396830b3084380f748f3dddf3c644e947cb24
hobiJeong <jjb26433@naver.com>
2023-12-04 14:25:56 +0900

    feat/#66: add swagger docs

--- src/apis/notice-boards/controllers/notice-boards.swagger.ts
+++ src/apis/notice-boards/controllers/notice-boards.swagger.ts
@@ -95,6 +95,9 @@ export const ApiNoticeBoard: ApiOperator<keyof NoticeBoardsController> = {
       HttpException.swaggerBuilder(HttpStatus.NOT_FOUND, [
         COMMON_ERROR_CODE.RESOURCE_NOT_FOUND,
       ]),
+      HttpException.swaggerBuilder(HttpStatus.INTERNAL_SERVER_ERROR, [
+        COMMON_ERROR_CODE.SERVER_ERROR,
+      ]),
     );
   },
 };

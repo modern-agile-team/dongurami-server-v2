953d09742cddfebd9288352e5e02e8bc141c2715
hobiJeong <jjb26433@naver.com>
2023-12-07 16:12:56 +0900

    modify/#66: modify extends

--- src/apis/notice-boards/dto/put-update-notice-board.dto.ts
+++ src/apis/notice-boards/dto/put-update-notice-board.dto.ts
@@ -1,14 +1,8 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { CreateNoticeBoardDto } from './create-notice-board.dto';
-import { IsBoolean } from 'class-validator';
 
 export class PutUpdateNoticeBoardDto extends PickType(CreateNoticeBoardDto, [
   'title',
   'description',
-] as const) {
-  @ApiProperty({
-    description: '댓글 허용 여부',
-  })
-  @IsBoolean()
-  isAllowComment: boolean;
-}
+  'isAllowComment',
+] as const) {}

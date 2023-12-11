ba60e76f8441ab8f01abe975e501abaa1d87c06c
LeeSeokHo <rrgks@naver.com>
2023-12-11 20:56:51 +0900

    feat: create free post comment api interface

--- src/apis/free-posts/free-post-comments/services/free-post-comments.service.ts
+++ src/apis/free-posts/free-post-comments/services/free-post-comments.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateFreePostCommentDto } from '@src/apis/free-posts/free-post-comments/dto/create-free-post-comment.dto';
+import { FindFreePostCommentListQueryDto } from '@src/apis/free-posts/free-post-comments/dto/find-free-post-comment-list-query.dto';
+import { FreePostCommentDto } from '@src/apis/free-posts/free-post-comments/dto/free-post-comment.dto';
+import { FreePostCommentsItemDto } from '@src/apis/free-posts/free-post-comments/dto/free-post-comments-item.dto';
+import { PutUpdateFreePostCommentDto } from '@src/apis/free-posts/free-post-comments/dto/put-update-free-post-comment.dto';
 import { FreePostCommentHistoryRepository } from '@src/apis/free-posts/free-post-comments/repositories/free-post-comment-history.repository';
 import { FreePostCommentRepository } from '@src/apis/free-posts/free-post-comments/repositories/free-post-comment.repository';
 
@@ -9,13 +14,34 @@ export class FreePostCommentsService {
     private readonly freePostCommentHistoryRepository: FreePostCommentHistoryRepository,
   ) {}
 
-  async create() {}
+  async create(
+    userId: number,
+    freePostId: number,
+    createFreePostCommentDto: CreateFreePostCommentDto,
+  ): Promise<FreePostCommentDto> {
+    return new FreePostCommentDto();
+  }
 
-  findAllAndCount() {}
+  async findAllAndCount(
+    findFreePostCommentListQueryDto: FindFreePostCommentListQueryDto,
+  ): Promise<[FreePostCommentsItemDto[], number]> {
+    return [[], 1];
+  }
 
-  async putUpdate() {}
+  async putUpdate(
+    userId: number,
+    freePostId: number,
+    freePostCommentId: number,
+    putUpdateFreePostCommentDto: PutUpdateFreePostCommentDto,
+  ): Promise<FreePostCommentDto> {
+    return new FreePostCommentDto();
+  }
 
-  async patchUpdate() {}
-
-  async remove() {}
+  async remove(
+    userId: number,
+    freePostId: number,
+    freePostCommentId: number,
+  ): Promise<number> {
+    return 1;
+  }
 }

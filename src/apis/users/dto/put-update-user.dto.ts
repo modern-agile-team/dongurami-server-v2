fa681616f5bb0bdc3e7fef4d3f69b8ed5d24aec4
hobiJeong <jjb26433@naver.com>
2024-01-04 17:24:15 +0900

    feat/#110: modify dto property

--- src/apis/users/dto/put-update-user.dto.ts
+++ src/apis/users/dto/put-update-user.dto.ts
@@ -1,1 +1,9 @@
-export class PutUpdateUserDto {}
+import { OmitType } from '@nestjs/swagger';
+import { CreateUserRequestBodyDto } from './create-user-request-body.dto';
+
+export class PutUpdateUserDto extends OmitType(CreateUserRequestBodyDto, [
+  'loginType',
+  'role',
+  'password',
+  'gender',
+]) {}

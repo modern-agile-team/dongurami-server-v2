1388a8dbf4f051d9f1bb586608965f74a887149f
ChangHoonOh <dhckdgns3@naver.com>
2024-02-04 15:14:32 +0900

    modify/QYOG-38 migrate user, user_history email type

---
+++ migrations/user/1706878330599-user-email-nullable.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm"
+
+export class UserEmailNullable1706878330599 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'user',
+      'email',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        isNullable: true,
+        comment: '이메일'
+      })
+    );
+
+    await queryRunner.changeColumn(
+      'user_history',
+      'email',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        isNullable: true,
+        comment: '이메일'
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'user',
+      'email',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        isNullable: false,
+        comment: '이메일'
+      })
+    );
+
+    await queryRunner.changeColumn(
+      'user_history',
+      'email',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        isNullable: false,
+        comment: '이메일'
+      })
+    );
+  }
+}

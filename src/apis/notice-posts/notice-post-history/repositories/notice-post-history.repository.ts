a08d27a7cbb742c11d23f6923170a9e05200aff5
LeeSeokHo <rrgks@naver.com>
2024-02-17 16:42:49 +0900

    delete: 사용하지 않는 코드 제거

--- src/apis/notice-posts/notice-post-history/repositories/notice-post-history.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-
-import { CustomRepository } from '@src/core/type-orm/decorators/custom-repository.decorator';
-import { NoticePostHistory } from '@src/entities/NoticePostHistory';
-
-@CustomRepository(NoticePostHistory)
-export class NoticePostHistoryRepository extends Repository<NoticePostHistory> {}

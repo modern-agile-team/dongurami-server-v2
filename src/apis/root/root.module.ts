8afcab9b25d662331376aeef9c9e18d2742f996e
hobiJeong <jjb26433@naver.com>
2024-02-20 15:56:41 +0900

    feat/QYOG-72: export root service
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/root/root.module.ts
+++ src/apis/root/root.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { RootController } from '@src/apis/root/controllers/root.controller';
 import { RootService } from '@src/apis/root/services/root.service';
 
 @Module({
-  controllers: [RootController],
   providers: [RootService],
+  exports: [RootService],
 })
 export class RootModule {}

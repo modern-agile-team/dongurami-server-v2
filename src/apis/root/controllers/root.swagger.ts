ac4905037bd3b6e150e3916bb93e22a800563ab5
hobiJeong <jjb26433@naver.com>
2024-02-20 15:57:19 +0900

    doc/QYOG-72: delete unused code
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/root/controllers/root.swagger.ts
+++ src/apis/root/controllers/root.swagger.ts
@@ -1,21 +1,4 @@
-import { applyDecorators } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
-import { OperationObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
-
 import { RootController } from '@src/apis/root/controllers/root.controller';
-import { ErrorCodeResponseDto } from '@src/apis/root/dto/error-code-response.dto';
 import { ApiOperator } from '@src/types/type';
 
-export const ApiRoot: ApiOperator<keyof RootController> = {
-  FindAllErrorCode: (
-    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
-      Partial<OperationObject>,
-  ): PropertyDecorator => {
-    return applyDecorators(
-      ApiOperation({
-        ...apiOperationOptions,
-      }),
-      ApiOkResponse({ type: ErrorCodeResponseDto }),
-    );
-  },
-};
+export const ApiRoot: ApiOperator<keyof RootController> = {};

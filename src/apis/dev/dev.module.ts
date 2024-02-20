c5a4d13b80478f8867fd61b50e648b4c33316ba9
hobiJeong <jjb26433@naver.com>
2024-02-20 15:56:10 +0900

    setting/QYOG-72: apply middleware at dev module
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/dev/dev.module.ts
+++ src/apis/dev/dev.module.ts
@@ -1,8 +1,30 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 
+import { AuthModule } from '@src/apis/auth/auth.module';
 import { DevController } from '@src/apis/dev/controllers/dev.controller';
+import { RootModule } from '@src/apis/root/root.module';
+import { UseDevelopmentMiddleware } from '@src/middlewares/use-development.middleware';
 
 @Module({
+  imports: [AuthModule, RootModule],
   controllers: [DevController],
 })
-export class DevModule {}
+export class DevModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UseDevelopmentMiddleware).forRoutes(
+      {
+        path: 'dev/access-token/:userId',
+        method: RequestMethod.GET,
+      },
+      {
+        path: 'dev/error-code',
+        method: RequestMethod.GET,
+      },
+    );
+  }
+}

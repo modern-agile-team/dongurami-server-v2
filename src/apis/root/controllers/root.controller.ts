754570a617932deb434ca4a1d57c0b79c001e8df
hobiJeong <jjb26433@naver.com>
2024-02-20 15:57:44 +0900

    delete/QYOG-72: delete unused api
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/root/controllers/root.controller.ts
+++ src/apis/root/controllers/root.controller.ts
@@ -1,20 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
 
-import { ApiRoot } from '@src/apis/root/controllers/root.swagger';
 import { RootService } from '@src/apis/root/services/root.service';
 
-@ApiTags('root')
 @Controller()
 export class RootController {
   constructor(private readonly rootService: RootService) {}
-
-  @ApiRoot.FindAllErrorCode({
-    summary: '개발용으로 생성된 에러코드 전체조회',
-    description: '서버에서 관리하는 에러코드를 조회합니다.',
-  })
-  @Get('error-code')
-  findAllErrorCode() {
-    return this.rootService.findAllErrorCode();
-  }
 }

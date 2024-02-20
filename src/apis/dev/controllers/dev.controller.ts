124aba97b5fd8f77eefa11c4fba90d9e3430e04c
hobiJeong <jjb26433@naver.com>
2024-02-20 15:54:38 +0900

    modify/QYOG-72: create dev controller
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/dev/controllers/dev.controller.ts
+++ src/apis/dev/controllers/dev.controller.ts
@@ -1,18 +1,36 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { AuthService } from '@src/apis/auth/services/auth.service';
 import { ApiDev } from '@src/apis/dev/controllers/dev.swagger';
+import { RootService } from '@src/apis/root/services/root.service';
+import { ApiCommonResponse } from '@src/decorators/swagger/api-common-response.swagger';
 import { ParsePositiveIntPipe } from '@src/pipes/parse-positive-int.pipe';
 
 @ApiTags('_dev')
 @Controller('dev')
 export class DevController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly rootService: RootService,
+  ) {}
 
-  @ApiDev.GetAccessToken({ summary: '개발용으로 생성된 accessToken 생성 api' })
+  @ApiDev.GetAccessToken({
+    summary: '개발용으로 생성된 accessToken 생성 api',
+    description: '개발 환경에서만 사용 가능',
+  })
+  @ApiCommonResponse([HttpStatus.INTERNAL_SERVER_ERROR])
   @Get('access-token/:userId')
   getAccessToken(@Param('userId', ParsePositiveIntPipe) userId: number) {
     return this.authService.generateToken({ id: userId });
+  }
+
+  @ApiDev.FindAllErrorCode({
+    summary: '개발용으로 생성된 에러코드 전체조회',
+    description: '서버에서 관리하는 에러코드를 조회합니다.',
+  })
+  @Get('error-code')
+  findAllErrorCode() {
+    return this.rootService.findAllErrorCode();
   }
 }

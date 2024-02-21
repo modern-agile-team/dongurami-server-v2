2c60029109cf45b3d297a287ed997ad437dac65e
hobiJeong <jjb26433@naver.com>
2024-02-21 11:40:43 +0900

    refactor/QYOG-72: modify middlewares routs
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/dev/dev.module.ts
+++ src/apis/dev/dev.module.ts
@@ -1,9 +1,4 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { AuthModule } from '@src/apis/auth/auth.module';
 import { DevController } from '@src/apis/dev/controllers/dev.controller';
@@ -16,15 +11,6 @@ import { UseDevelopmentMiddleware } from '@src/middlewares/use-development.middl
 })
 export class DevModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(UseDevelopmentMiddleware).forRoutes(
-      {
-        path: 'dev/access-token/:userId',
-        method: RequestMethod.GET,
-      },
-      {
-        path: 'dev/error-code',
-        method: RequestMethod.GET,
-      },
-    );
+    consumer.apply(UseDevelopmentMiddleware).forRoutes(DevController);
   }
 }

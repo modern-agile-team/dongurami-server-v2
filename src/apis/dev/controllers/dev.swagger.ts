d85ca74b46d60d81b704e72f7c8cadebf70d006f
hobiJeong <jjb26433@naver.com>
2024-02-21 11:48:58 +0900

    modify/QYOG-72: modify swagger
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/dev/controllers/dev.swagger.ts
+++ src/apis/dev/controllers/dev.swagger.ts
@@ -4,7 +4,6 @@ import { OperationObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.i
 
 import { DevController } from '@src/apis/dev/controllers/dev.controller';
 import { ErrorCodeResponseDto } from '@src/apis/root/dto/error-code-response.dto';
-import { AUTH_ERROR_CODE } from '@src/constants/error/auth/auth-error-code.constant';
 import { COMMON_ERROR_CODE } from '@src/constants/error/common/common-error-code.constant';
 import { HttpException } from '@src/http-exceptions/exceptions/http.exception';
 import { ApiOperator } from '@src/types/type';
@@ -21,11 +20,7 @@ export const ApiDev: ApiOperator<keyof DevController> = {
       }),
       HttpException.swaggerBuilder(
         HttpStatus.BAD_REQUEST,
-        [
-          COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER,
-          AUTH_ERROR_CODE.ACCOUNT_NOT_FOUND,
-          AUTH_ERROR_CODE.DIFFERENT_ACCOUNT_INFORMATION,
-        ],
+        [COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER],
         {
           description:
             '해당 필드는 request parameter 가 잘못된 경우에만 리턴됩니다.',
@@ -35,12 +30,8 @@ export const ApiDev: ApiOperator<keyof DevController> = {
       ApiOkResponse({
         description: '발급된 access-token',
         schema: {
-          properties: {
-            accessToken: {
-              type: 'string',
-              description: '발급된 accessToken',
-            },
-          },
+          type: 'string',
+          description: '발급된 accessToken',
         },
       }),
     );

71387034e58f135aa4db46ed6612cc50891aa9b5
hobiJeong <jjb26433@naver.com>
2024-02-21 11:41:58 +0900

    refactor/QYOG-72: create dev service
    https://dongurami.atlassian.net/browse/QYOG-72

--- src/apis/dev/dev.module.ts
+++ src/apis/dev/dev.module.ts
@@ -2,12 +2,14 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { AuthModule } from '@src/apis/auth/auth.module';
 import { DevController } from '@src/apis/dev/controllers/dev.controller';
+import { DevService } from '@src/apis/dev/dev.service';
 import { RootModule } from '@src/apis/root/root.module';
 import { UseDevelopmentMiddleware } from '@src/middlewares/use-development.middleware';
 
 @Module({
   imports: [AuthModule, RootModule],
   controllers: [DevController],
+  providers: [DevService],
 })
 export class DevModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {

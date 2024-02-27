b37a8a8fb9fa7174964a6089f27e1bba137f0754
hobiJeong <jjb26433@naver.com>
2024-02-28 01:32:42 +0900

    setting/QYOG-90: add aws-specific constants
    https://dongurami.atlassian.net/browse/QYOG-90

--- src/core/app-config/constants/app-config.constant.ts
+++ src/core/app-config/constants/app-config.constant.ts
@@ -10,10 +10,16 @@ const JWT = {
   JWT_SECRET: 'JWT_SECRET',
 } as const;
 
+const AWS = {
+  AWS_S3_ACCESS_KEY: 'AWS_S3_ACCESS_KEY',
+  AWS_S3_SECRET_KEY: 'AWS_S3_SECRET_KEY',
+} as const;
+
 export const ENV_KEY = {
   PORT: 'PORT',
   NODE_ENV: 'NODE_ENV',
   DOMAIN: 'DOMAIN',
   ...RDB,
   ...JWT,
+  ...AWS,
 } as const;

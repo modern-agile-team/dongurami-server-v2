ba031f71c24eec4e9123cdccc9b1a9bec1f579da
hobiJeong <jjb26433@naver.com>
2024-03-03 19:01:30 +0900

    delete/QYOG-90: delete useless code
    https://dongurami.atlassian.net/browse/QYOG-90

--- src/s3/s3.module.ts
+++ src/s3/s3.module.ts
@@ -5,6 +5,6 @@ import { S3Service } from '@src/s3/services/s3.service';
 
 @Module({
   providers: [...S3Provider, S3Service],
-  exports: [...S3Provider, S3Service],
+  exports: [S3Service],
 })
 export class S3Module {}

b3ebfe9c4ddd32889a0b95855eb7c2f6dbf66653
hobiJeong <jjb26433@naver.com>
2024-03-03 22:54:36 +0900

    refactor/QYOG-90: modify folder name
    https://dongurami.atlassian.net/browse/QYOG-90

--- src/apis/attachments/attachments.module.ts
+++ src/apis/attachments/attachments.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { MemoryStoredFile, NestjsFormDataModule } from 'nestjs-form-data';
 
 import { AttachmentsController } from '@src/apis/attachments/controllers/attachments.controller';
-import { AttachmentRepository } from '@src/apis/attachments/repository/attachment.repository';
+import { AttachmentRepository } from '@src/apis/attachments/repositories/attachment.repository';
 import { AttachmentsService } from '@src/apis/attachments/services/attachments.service';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 import { S3Module } from '@src/s3/s3.module';

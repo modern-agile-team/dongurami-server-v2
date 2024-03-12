1c38eaf541d1eef022a68f78f0827bb4339d6a84
hobiJeong <jjb26433@naver.com>
2024-03-12 13:45:58 +0900

    refactor/QYOG-95: modify 400 error schema ValidationError --> CustomValidationError
    https://dongurami.atlassian.net/browse/QYOG-95

--- src/apis/attachments/controllers/attachments.swagger.ts
+++ src/apis/attachments/controllers/attachments.swagger.ts
@@ -2,13 +2,12 @@ import { HttpStatus, applyDecorators } from '@nestjs/common';
 import { ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
 import { OperationObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
-import { ValidationError } from 'class-validator';
-
 import { AttachmentsController } from '@src/apis/attachments/controllers/attachments.controller';
 import { AttachmentDto } from '@src/apis/attachments/dto/attachment.dto';
 import { COMMON_ERROR_CODE } from '@src/constants/error/common/common-error-code.constant';
 import { HttpException } from '@src/http-exceptions/exceptions/http.exception';
 import { CommonResponseDto } from '@src/interceptors/success-interceptor/dto/common-response.dto';
+import { CustomValidationError } from '@src/types/custom-validation-errors.type';
 import { ApiOperator } from '@src/types/type';
 
 export const ApiAttachments: ApiOperator<keyof AttachmentsController> = {
@@ -48,7 +47,7 @@ export const ApiAttachments: ApiOperator<keyof AttachmentsController> = {
         {
           description:
             '해당 필드는 request parameter 가 잘못된 경우에만 리턴됩니다.',
-          type: ValidationError,
+          type: CustomValidationError,
         },
       ),
     );

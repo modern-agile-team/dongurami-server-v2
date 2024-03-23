7c0d7c7956ad30ea88a1da8d3ff7dcefa72ddf2f
LeeSeokHo <rrgks@naver.com>
2024-03-23 17:03:01 +0900

    feat/QYOG-93: 공지게시글 전체조회 응답에 user 추가
    https://dongurami.atlassian.net/browse/QYOG-93

--- src/apis/notice-posts/dto/notice-posts-item.dto.ts
+++ src/apis/notice-posts/dto/notice-posts-item.dto.ts
@@ -1,7 +1,16 @@
-import { OmitType } from '@nestjs/swagger';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+
+import { Type } from 'class-transformer';
 
 import { NoticePostDto } from '@src/apis/notice-posts/dto/notice-post.dto';
+import { UserDto } from '@src/apis/users/dto/user.dto';
 
 export class NoticePostsItemDto extends OmitType(NoticePostDto, [
   'description',
-] as const) {}
+] as const) {
+  @ApiProperty({
+    description: '게시글 작성 유저 정보',
+  })
+  @Type(() => UserDto)
+  user: UserDto;
+}

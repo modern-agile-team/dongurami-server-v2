487b04e83bb1b3de57a939447181998fabc12456
hobiJeong <jjb26433@naver.com>
2024-03-26 18:45:22 +0900

    refactor/QYOG-113: default 값에 따라 다르게 작동하는 boolean transformer 생성
    https://dongurami.atlassian.net/browse/QYOG-113

--- src/entities/transformers/default-true-boolean.transformer.ts
+++ src/entities/transformers/default-true-boolean.transformer.ts
@@ -1,11 +1,11 @@
 import { ValueTransformer } from 'typeorm';
 
-export class BooleanTransformer implements ValueTransformer {
+export class DefaultTrueBooleanTransformer implements ValueTransformer {
   public from(value?: number | null): boolean | undefined {
     return !!value;
   }
 
   public to(value?: boolean | null): number | undefined {
-    return Number(value);
+    return Number(value) || 1;
   }
 }

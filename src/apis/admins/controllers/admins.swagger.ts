6c5167656d64d67a74353d393e9f1d3e1fd71959
hobiJeong <jjb26433@naver.com>
2024-03-27 14:29:01 +0900

    doc/QYOG-111: 스웨거 추가(미완)
    https://dongurami.atlassian.net/browse/QYOG-111

--- src/apis/admins/controllers/admins.swagger.ts
+++ src/apis/admins/controllers/admins.swagger.ts
@@ -3,13 +3,14 @@ import { ApiOperation } from '@nestjs/swagger';
 import { OperationObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
 import { AdminsController } from '@src/apis/admins/controllers/admins.controller';
+import { ClubDto } from '@src/apis/clubs/dto/club.dto';
 import { MajorDto } from '@src/apis/major/dto/major.dto';
 import { COMMON_ERROR_CODE } from '@src/constants/error/common/common-error-code.constant';
 import { MAJOR_ERROR_CODE } from '@src/constants/error/major/major-error-code.constant';
 import { HttpException } from '@src/http-exceptions/exceptions/http.exception';
 import { DetailResponseDto } from '@src/interceptors/success-interceptor/dto/detail-response.dto';
-import { ApiOperator } from '@src/types/type';
 import { CustomValidationError } from '@src/types/custom-validation-errors.type';
+import { ApiOperator } from '@src/types/type';
 
 export const ApiAdmins: ApiOperator<keyof AdminsController> = {
   CreateNewMajor: function (
@@ -21,6 +22,31 @@ export const ApiAdmins: ApiOperator<keyof AdminsController> = {
         ...apiOperationOptions,
       }),
       DetailResponseDto.swaggerBuilder(HttpStatus.CREATED, 'major', MajorDto),
+      HttpException.swaggerBuilder(
+        HttpStatus.BAD_REQUEST,
+        [COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER],
+        {
+          description:
+            '해당 필드는 request parameter 가 잘못된 경우에만 리턴됩니다.',
+          type: CustomValidationError,
+        },
+      ),
+      HttpException.swaggerBuilder(HttpStatus.CONFLICT, [
+        MAJOR_ERROR_CODE.ALREADY_EXIST_MAJOR_NAME,
+        MAJOR_ERROR_CODE.ALREADY_EXIST_MAJOR_CODE,
+      ]),
+    );
+  },
+
+  CreateNewClub: function (
+    apiOperationOptions: Required<Pick<Partial<OperationObject>, 'summary'>> &
+      Partial<OperationObject>,
+  ): PropertyDecorator {
+    return applyDecorators(
+      ApiOperation({
+        ...apiOperationOptions,
+      }),
+      DetailResponseDto.swaggerBuilder(HttpStatus.CREATED, 'club', ClubDto),
       HttpException.swaggerBuilder(
         HttpStatus.BAD_REQUEST,
         [COMMON_ERROR_CODE.INVALID_REQUEST_PARAMETER],

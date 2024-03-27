b37f8a2ffbe9ac0599b057002b4ac91e8acdf105
hobiJeong <jjb26433@naver.com>
2024-03-27 14:27:34 +0900

    feat/QYOG-111: 클럽 모듈에 클럽 태그 및 카테고리 module import, export servic
    https://dongurami.atlassian.net/browse/QYOG-111

--- src/apis/clubs/clubs.module.ts
+++ src/apis/clubs/clubs.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { ClubCategoriesModule } from '@src/apis/club-categories/club-categories.module';
 import { ClubCategoryLinksModule } from '@src/apis/club-category-links/club-category-links.module';
 import { ClubTagLinksModule } from '@src/apis/club-tag-links/club-tag-links.module';
+import { ClubTagsModule } from '@src/apis/club-tags/club-tags.module';
 import { ClubsController } from '@src/apis/clubs/controllers/clubs.controller';
 import { ClubRepository } from '@src/apis/clubs/repositories/club.repository';
 import { ClubsService } from '@src/apis/clubs/services/clubs.service';
@@ -13,8 +15,11 @@ import { QueryHelper } from '@src/helpers/query.helper';
     TypeOrmExModule.forCustomRepository([ClubRepository]),
     ClubTagLinksModule,
     ClubCategoryLinksModule,
+    ClubTagsModule,
+    ClubCategoriesModule,
   ],
   controllers: [ClubsController],
   providers: [ClubsService, QueryHelper],
+  exports: [ClubsService],
 })
 export class ClubsModule {}

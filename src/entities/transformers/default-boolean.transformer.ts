695f857b37c320778fdf5c180968622be19409b0
hobiJeong <jjb26433@naver.com>
2024-03-27 16:07:47 +0900

    refactor/QYOG-113: modify type
    https://dongurami.atlassian.net/browse/QYOG-113

--- src/entities/transformers/default-boolean.transformer.ts
+++ src/entities/transformers/default-boolean.transformer.ts
@@ -1,13 +1,13 @@
 import { ValueTransformer } from 'typeorm';
 
 export class DefaultBooleanTransformer implements ValueTransformer {
-  constructor(private readonly defaultValue: boolean) {}
+  constructor(private readonly defaultValue?: boolean) {}
 
-  public from(value?: number | null): boolean | undefined {
+  public from(value?: number): boolean {
     return !!value;
   }
 
-  public to(value?: boolean | null | undefined): number {
+  public to(value?: boolean | undefined): number {
     return Number(value ?? this.defaultValue);
   }
 }

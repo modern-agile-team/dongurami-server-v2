053734ecae6afd3400cd8a87cdeb5ba3ec8f19fe
hobiJeong <jjb26433@naver.com>
2024-03-27 14:27:59 +0900

    feat/QYOG-111: 어드민 모듈에 클럽 모듈 import
    https://dongurami.atlassian.net/browse/QYOG-111

--- src/apis/admins/admins.module.ts
+++ src/apis/admins/admins.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AdminsController } from '@src/apis/admins/controllers/admins.controller';
+import { ClubsModule } from '@src/apis/clubs/clubs.module';
 import { MajorModule } from '@src/apis/major/major.module';
 
 @Module({
-  imports: [MajorModule],
+  imports: [MajorModule, ClubsModule],
   controllers: [AdminsController],
 })
 export class AdminsModule {}

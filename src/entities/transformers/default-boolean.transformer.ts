cd4896ecfdef8418ace53a9a213c850091b64c55
hobiJeong <jjb26433@naver.com>
2024-03-27 19:52:21 +0900

    feat/QYOG-113: value가 null일 경우도 고려
    https://dongurami.atlassian.net/browse/QYOG-113

--- src/entities/transformers/default-boolean.transformer.ts
+++ src/entities/transformers/default-boolean.transformer.ts
@@ -3,11 +3,13 @@ import { ValueTransformer } from 'typeorm';
 export class DefaultBooleanTransformer implements ValueTransformer {
   constructor(private readonly defaultValue: boolean) {}
 
-  public from(value?: number): boolean {
-    return !!value;
+  public from(value: number | null): boolean | null {
+    console.log(value);
+
+    return value === null ? null : !!value;
   }
 
-  public to(value?: boolean | undefined): number {
+  public to(value?: boolean): number {
     return Number(value ?? this.defaultValue);
   }
 }

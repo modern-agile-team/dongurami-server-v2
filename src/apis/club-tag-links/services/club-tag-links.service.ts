ebb5e46ce1b5b696de71b2d8fa7ca1d96ef18dcf
hobiJeong <jjb26433@naver.com>
2024-03-28 17:32:02 +0900

    feat/QYOG-111: create club category link dto 이름 수정
    https://dongurami.atlassian.net/browse/QYOG-111

--- src/apis/club-tag-links/services/club-tag-links.service.ts
+++ src/apis/club-tag-links/services/club-tag-links.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
-import { CreateClubTagLinksDto } from '@src/apis/club-tag-links/dto/create-club-tag-links.dto';
+import { CreateClubTagLinkDto } from '@src/apis/club-tag-links/dto/create-club-tag-link.dto';
 import { ClubTagLinkRepository } from '@src/apis/club-tag-links/repositories/club-tag-link.repository';
 
 @Injectable()
 export class ClubTagLinksService {
   constructor(private readonly clubTagLinksRepository: ClubTagLinkRepository) {}
 
-  async create(createClubTagLinksDto: CreateClubTagLinksDto[]): Promise<void> {
+  async create(createClubTagLinksDto: CreateClubTagLinkDto[]): Promise<void> {
     await this.clubTagLinksRepository.save(
       this.clubTagLinksRepository.create(createClubTagLinksDto),
       { reload: false },

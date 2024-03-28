577061ff23d9478ba4bcf0c824b45340b113f510
hobiJeong <jjb26433@naver.com>
2024-03-28 21:27:31 +0900

    feat/QYOG-100: club category order field
    https://dongurami.atlassian.net/browse/QYOG-100

--- src/apis/club-categories/constants/club-category.constant.ts
+++ src/apis/club-categories/constants/club-category.constant.ts
@@ -1,3 +1,11 @@
+import { ClubCategoryDto } from '@src/apis/club-categories/dto/club-category.dto';
+
+export const CLUB_CATEGORY_ORDER_FIELD: readonly (keyof ClubCategoryDto)[] = [
+  'id',
+  'name',
+  'createdAt',
+] as const;
+
 export const CLUB_CATEGORY_NAME = {
   MIN: 1,
   MAX: 20,

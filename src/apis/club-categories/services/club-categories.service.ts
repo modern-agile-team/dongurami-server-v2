34c1b16dbf121e43db8a40901c68519d49ee6ce5
hobiJeong <jjb26433@naver.com>
2024-03-28 20:47:40 +0900

    feat/QYOG-99: club category 추가 metohd 생성
    https://dongurami.atlassian.net/browse/QYOG-99

--- src/apis/club-categories/services/club-categories.service.ts
+++ src/apis/club-categories/services/club-categories.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+import { ClubCategoryDto } from '@src/apis/club-categories/dto/club-category.dto';
 import { CreateClubCategoryRequestBodyDto } from '@src/apis/club-categories/dto/create-club-category-request-body.dto';
 import { ClubCategoryRepository } from '@src/apis/club-categories/repositories/club-category.repository';
+import { CLUB_CATEGORY_ERROR_CODE } from '@src/constants/error/club-category/club-category-error-code.constant';
+import { ClubCategory } from '@src/entities/ClubCategory';
+import { HttpConflictException } from '@src/http-exceptions/exceptions/http-conflict.exception';
 
 @Injectable()
 export class ClubCategoriesService {
@@ -9,5 +13,30 @@ export class ClubCategoriesService {
     private readonly clubCategoryRepository: ClubCategoryRepository,
   ) {}
 
-  create(createClubCategoryRequestBodyDto: CreateClubCategoryRequestBodyDto) {}
+  async create(
+    userId: number,
+    createClubCategoryRequestBodyDto: CreateClubCategoryRequestBodyDto,
+  ): Promise<ClubCategoryDto> {
+    const { name, memo } = createClubCategoryRequestBodyDto;
+
+    const existClubCategory: Pick<ClubCategory, 'id' | 'name'> =
+      await this.clubCategoryRepository.findOne({
+        select: ['name'],
+        where: { name },
+      });
+
+    if (existClubCategory) {
+      throw new HttpConflictException({
+        code: CLUB_CATEGORY_ERROR_CODE.ALREADY_EXIST_CLUB_CATEGORY_NAME,
+      });
+    }
+
+    const newClubCategory = await this.clubCategoryRepository.save({
+      userId,
+      name,
+      memo,
+    });
+
+    return new ClubCategoryDto(newClubCategory);
+  }
 }

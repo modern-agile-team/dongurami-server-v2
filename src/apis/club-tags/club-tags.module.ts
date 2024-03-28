d9245442379cfb2d0c31b03dedac54ad3bc5dfdb
hobiJeong <jjb26433@naver.com>
2024-03-28 17:32:39 +0900

    setting/QYOG-111: 클럽 태그 링크 모듈 및 클럽 모듈 참조, 클럽 태그 서비스 생성, 클럽 태그 및 링크 생성 메서드 추가
    https://dongurami.atlassian.net/browse/QYOG-111

--- src/apis/club-tags/club-tags.module.ts
+++ src/apis/club-tags/club-tags.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 
+import { ClubTagLinksModule } from '@src/apis/club-tag-links/club-tag-links.module';
 import { ClubTagRepository } from '@src/apis/club-tags/repositories/club-tag.repository';
+import { ClubTagsService } from '@src/apis/club-tags/services/club-tags.service';
+import { ClubsModule } from '@src/apis/clubs/clubs.module';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([ClubTagRepository])],
-  exports: [TypeOrmExModule],
+  imports: [
+    forwardRef(() => ClubsModule),
+    ClubTagLinksModule,
+    TypeOrmExModule.forCustomRepository([ClubTagRepository]),
+  ],
+  providers: [ClubTagsService],
+  exports: [ClubTagsService, TypeOrmExModule],
 })
 export class ClubTagsModule {}

07f1c3b9847f736440a776e6370a67b2089fffd4
hobiJeong <jjb26433@naver.com>
2024-03-30 00:19:12 +0900

    delete/QYOG-111: club tag links service 삭제
    https://dongurami.atlassian.net/browse/QYOG-111

--- src/apis/club-tag-links/club-tag-links.module.ts
+++ src/apis/club-tag-links/club-tag-links.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { ClubTagLinkRepository } from '@src/apis/club-tag-links/repositories/club-tag-link.repository';
-import { ClubTagLinksService } from '@src/apis/club-tag-links/services/club-tag-links.service';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([ClubTagLinkRepository])],
-  providers: [ClubTagLinksService],
-  exports: [ClubTagLinksService, TypeOrmExModule],
+  exports: [TypeOrmExModule],
 })
 export class ClubTagLinksModule {}

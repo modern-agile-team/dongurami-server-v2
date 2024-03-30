1cbc59b8962788766cdfd8fc2fdf0d55c79b712f
LeeSeokHo <rrgks@naver.com>
2024-03-30 15:50:08 +0900

    feat/QYOG-105: 공지 게시글 생성 시 태그 추가
    https://dongurami.atlassian.net/browse/QYOG-105

--- src/apis/notice-posts/dto/notice-posts-item.dto.ts
+++ src/apis/notice-posts/dto/notice-posts-item.dto.ts
@@ -7,6 +7,7 @@ import { UserDto } from '@src/apis/users/dto/user.dto';
 
 export class NoticePostsItemDto extends OmitType(NoticePostDto, [
   'description',
+  'postTags',
 ] as const) {
   @ApiProperty({
     description: '게시글 작성 유저 정보',

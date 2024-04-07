57362784a2c107da09f85ed6a81f8cdfa6774a98
LeeSeokHo <rrgks@naver.com>
2024-04-07 19:34:30 +0900

    db/QYOG-120: migration clubMember
    https://dongurami.atlassian.net/browse/QYOG-120

--- src/entities/Club.ts
+++ src/entities/Club.ts
@@ -10,6 +10,7 @@ import {
 import { ClubStatus } from '@src/apis/clubs/constants/club.enum';
 import { ClubCategoryLink } from '@src/entities/ClubCategoryLink';
 import { ClubHistory } from '@src/entities/ClubHistory';
+import { ClubMember } from '@src/entities/ClubMember';
 import { ClubTagLink } from '@src/entities/ClubTagLink';
 import { User } from '@src/entities/User';
 
@@ -89,6 +90,9 @@ export class Club {
 
   @OneToMany(() => ClubTagLink, (clubTagLink) => clubTagLink.club)
   clubTagLinks: ClubTagLink[];
+
+  @OneToMany(() => ClubMember, (clubMember) => clubMember.club)
+  clubMembers: ClubMember[];
 
   @OneToMany(
     () => ClubCategoryLink,

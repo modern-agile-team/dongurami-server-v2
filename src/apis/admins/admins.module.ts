3d27193418ef8569b24044cff6086d702e13589e
hobiJeong <137868493+hobiJeong@users.noreply.github.com>
2024-04-08 18:24:35 +0900

    Create club category api (#158)

--- src/apis/admins/admins.module.ts
+++ src/apis/admins/admins.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AdminsController } from '@src/apis/admins/controllers/admins.controller';
+import { ClubCategoriesModule } from '@src/apis/club-categories/club-categories.module';
 import { ClubsModule } from '@src/apis/clubs/clubs.module';
 import { MajorModule } from '@src/apis/major/major.module';
 
 @Module({
-  imports: [MajorModule, ClubsModule],
+  imports: [MajorModule, ClubsModule, ClubCategoriesModule],
   controllers: [AdminsController],
 })
 export class AdminsModule {}

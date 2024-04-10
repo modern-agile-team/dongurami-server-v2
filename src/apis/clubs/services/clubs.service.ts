b1b0a120ff7860e7323ffbafd4d995d347525d00
LeeSeokHo <rrgks@naver.com>
2024-04-10 17:24:06 +0900

    feat/QYOG-122: 최신 동아리 지원서 폼 조회 API
    https://dongurami.atlassian.net/browse/QYOG-122

--- src/apis/clubs/services/clubs.service.ts
+++ src/apis/clubs/services/clubs.service.ts
@@ -5,6 +5,7 @@ import { differenceWith } from 'lodash';
 import { In } from 'typeorm';
 import { Transactional } from 'typeorm-transactional';
 
+import { ClubApplicationFormDto } from '@src/apis/club-application-form/dto/club-application-form.dto';
 import { CreateClubApplicationFormDto } from '@src/apis/club-application-form/dto/create-club-application-form.dto';
 import { ClubApplicationFormService } from '@src/apis/club-application-form/services/club-application-form.service';
 import { ClubCategoryDto } from '@src/apis/club-categories/dto/club-category.dto';
@@ -29,6 +30,7 @@ import { Club } from '@src/entities/Club';
 import { ClubCategoryLink } from '@src/entities/ClubCategoryLink';
 import { ClubTagLink } from '@src/entities/ClubTagLink';
 import { QueryHelper } from '@src/helpers/query.helper';
+import { HttpInternalServerErrorException } from '@src/http-exceptions/exceptions/http-internal-server-error.exception';
 import { HttpNotFoundException } from '@src/http-exceptions/exceptions/http-not-found.exception';
 import { HttpUnprocessableEntityException } from '@src/http-exceptions/exceptions/http-unprocessable-entity.exception';
 
@@ -415,5 +417,32 @@ export class ClubsService {
         createdAt,
       });
     });
+  }
+
+  async findLatestApplicationForm(
+    clubId: number,
+  ): Promise<ClubApplicationFormDto> {
+    const isExistClub = await this.clubRepository.exist({
+      where: { id: clubId },
+    });
+
+    if (!isExistClub) {
+      throw new HttpNotFoundException({
+        code: COMMON_ERROR_CODE.RESOURCE_NOT_FOUND,
+      });
+    }
+
+    const latestApplicationForm =
+      await this.clubApplicationFormService.findLatestByClubId(clubId);
+
+    if (latestApplicationForm === undefined) {
+      throw new HttpInternalServerErrorException({
+        code: COMMON_ERROR_CODE.SERVER_ERROR,
+        ctx: '동아리는 존재하지만 동아리 지원서 폼이 존재하지 않음',
+        stack: new Error().stack,
+      });
+    }
+
+    return latestApplicationForm;
   }
 }

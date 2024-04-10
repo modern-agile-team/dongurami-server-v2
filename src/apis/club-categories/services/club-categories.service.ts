67b95ec01723c1b748019e0188889bcdc18f0ff7
hobiJeong <jjb26433@naver.com>
2024-04-10 21:08:39 +0900

    feat/QYOG-100: club category 전체 조회 메서드 추가
    https://dongurami.atlassian.net/browse/QYOG-100

--- src/apis/club-categories/services/club-categories.service.ts
+++ src/apis/club-categories/services/club-categories.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 import { ClubCategoryDto } from '@src/apis/club-categories/dto/club-category.dto';
 import { CreateClubCategoryRequestBodyDto } from '@src/apis/club-categories/dto/create-club-category-request-body.dto';
+import { FindClubCategoryListQueryDto } from '@src/apis/club-categories/dto/find-club-category-list-query.dto';
 import { ClubCategoryRepository } from '@src/apis/club-categories/repositories/club-category.repository';
 import { CLUB_CATEGORY_ERROR_CODE } from '@src/constants/error/club-category/club-category-error-code.constant';
 import { ClubCategory } from '@src/entities/ClubCategory';
+import { QueryHelper } from '@src/helpers/query.helper';
 import { HttpConflictException } from '@src/http-exceptions/exceptions/http-conflict.exception';
 
 @Injectable()
 export class ClubCategoriesService {
+  private readonly LIKE_SEARCH_FIELD: readonly (keyof Pick<
+    ClubCategoryDto,
+    'name'
+  >)[] = ['name'];
+
   constructor(
     private readonly clubCategoryRepository: ClubCategoryRepository,
+    private readonly queryHelper: QueryHelper,
   ) {}
 
   async create(
@@ -40,5 +48,19 @@ export class ClubCategoriesService {
     return new ClubCategoryDto(newClubCategory);
   }
 
-  findAll() {}
+  async findAll(
+    findClubCategoryListQueryDto: FindClubCategoryListQueryDto,
+  ): Promise<ClubCategory[]> {
+    const { order, ...filter } = findClubCategoryListQueryDto;
+
+    const where = this.queryHelper.buildWherePropForFind(
+      filter,
+      this.LIKE_SEARCH_FIELD,
+    );
+
+    return this.clubCategoryRepository.find({
+      where,
+      order,
+    });
+  }
 }

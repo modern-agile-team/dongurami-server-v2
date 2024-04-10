014fa776a5c05168825a53663fb06c08f39f4010
hobiJeong <jjb26433@naver.com>
2024-04-10 21:27:29 +0900

    delete/QYOG-100: useless class
    https://dongurami.atlassian.net/browse/QYOG-100

--- src/apis/club-categories/dto/club-category-item.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ClubCategoryDto } from '@src/apis/club-categories/dto/club-category.dto';
-
-export class ClubCategoryItemDto extends ClubCategoryDto {
-  constructor(clubCategoryItemDto: Partial<ClubCategoryItemDto> = {}) {
-    super();
-
-    Object.assign(this, clubCategoryItemDto);
-  }
-}

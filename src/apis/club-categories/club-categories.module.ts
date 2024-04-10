48856281f3b246573e2afe629674f8abc401b42b
hobiJeong <jjb26433@naver.com>
2024-04-10 21:06:34 +0900

    feat/QYOG-100: query helper providers 배열에 등록
    https://dongurami.atlassian.net/browse/QYOG-100

--- src/apis/club-categories/club-categories.module.ts
+++ src/apis/club-categories/club-categories.module.ts
@@ -4,11 +4,12 @@ import { ClubCategoriesController } from '@src/apis/club-categories/controllers/
 import { ClubCategoryRepository } from '@src/apis/club-categories/repositories/club-category.repository';
 import { ClubCategoriesService } from '@src/apis/club-categories/services/club-categories.service';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
+import { QueryHelper } from '@src/helpers/query.helper';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([ClubCategoryRepository])],
   controllers: [ClubCategoriesController],
-  providers: [ClubCategoriesService],
+  providers: [ClubCategoriesService, QueryHelper],
   exports: [ClubCategoriesService, TypeOrmExModule],
 })
 export class ClubCategoriesModule {}

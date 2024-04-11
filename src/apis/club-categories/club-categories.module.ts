a69e351bd2d36042b8a2dbd4271966fa66911adc
hobiJeong <137868493+hobiJeong@users.noreply.github.com>
2024-04-11 17:44:59 +0900

    모든 동아리 카테고리 전체 조회 API (#175)

--- src/apis/club-categories/club-categories.module.ts
+++ src/apis/club-categories/club-categories.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 
+import { ClubCategoriesController } from '@src/apis/club-categories/controllers/club-categories.controller';
 import { ClubCategoryRepository } from '@src/apis/club-categories/repositories/club-category.repository';
 import { ClubCategoriesService } from '@src/apis/club-categories/services/club-categories.service';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
+import { QueryHelper } from '@src/helpers/query.helper';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([ClubCategoryRepository])],
-  providers: [ClubCategoriesService],
+  controllers: [ClubCategoriesController],
+  providers: [ClubCategoriesService, QueryHelper],
   exports: [ClubCategoriesService, TypeOrmExModule],
 })
 export class ClubCategoriesModule {}
--- src/apis/club-categories/services/club-categories.service.ts
+++ src/apis/club-categories/services/club-categories.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 import { ClubCategoryDto } from '@src/apis/club-categories/dto/club-category.dto';
 import { CreateClubCategoryRequestBodyDto } from '@src/apis/club-categories/dto/create-club-category-request-body.dto';
+import { FindClubCategoryListQueryDto } from '@src/apis/club-categories/dto/find-club-category-list-query.dto';
 import { ClubCategoryRepository } from '@src/apis/club-categories/repositories/club-category.repository';
 import { CLUB_CATEGORY_ERROR_CODE } from '@src/constants/error/club-category/club-category-error-code.constant';
 import { ClubCategory } from '@src/entities/ClubCategory';
+import { QueryHelper } from '@src/helpers/query.helper';
 import { HttpConflictException } from '@src/http-exceptions/exceptions/http-conflict.exception';
 
 @Injectable()
 export class ClubCategoriesService {
+  private readonly LIKE_SEARCH_FIELD: readonly (keyof Pick<
+    ClubCategoryDto,
+    'name'
+  >)[] = ['name'];
+
   constructor(
     private readonly clubCategoryRepository: ClubCategoryRepository,
+    private readonly queryHelper: QueryHelper,
   ) {}
 
   async create(
@@ -38,5 +46,26 @@ export class ClubCategoriesService {
     });
 
     return new ClubCategoryDto(newClubCategory);
+  }
+
+  async findAll(
+    findClubCategoryListQueryDto: FindClubCategoryListQueryDto,
+  ): Promise<ClubCategoryDto[]> {
+    const { order, ...filter } = findClubCategoryListQueryDto;
+
+    const where = this.queryHelper.buildWherePropForFind(
+      filter,
+      this.LIKE_SEARCH_FIELD,
+    );
+
+    const clubCategories = await this.clubCategoryRepository.find({
+      where,
+      order,
+    });
+
+    return clubCategories.map((clubCategory) => {
+      const { id, userId, name, createdAt, updatedAt } = clubCategory;
+      return new ClubCategoryDto({ id, userId, name, createdAt, updatedAt });
+    });
   }
 }

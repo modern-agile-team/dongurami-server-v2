52e16a0f4c68fedbe9a4fb0784f2a01cdcabc2ec
hobiJeong <jjb26433@naver.com>
2024-04-11 23:14:45 +0900

    feat/QYOG-124: create club posts module
    https://dongurami.atlassian.net/browse/QYOG-124

--- src/apis/api.module.ts
+++ src/apis/api.module.ts
@@ -7,6 +7,7 @@ import { AuthSocialModule } from '@src/apis/auth/social/auth-social.module';
 import { ClubCategoriesModule } from '@src/apis/club-categories/club-categories.module';
 import { ClubCategoryLinksModule } from '@src/apis/club-category-links/club-category-links.module';
 import { ClubMembersModule } from '@src/apis/club-members/club-members.module';
+import { ClubPostsModule } from '@src/apis/club-posts/club-posts.module';
 import { ClubTagLinksModule } from '@src/apis/club-tag-links/club-tag-links.module';
 import { ClubTagsModule } from '@src/apis/club-tags/club-tags.module';
 import { ClubsModule } from '@src/apis/clubs/clubs.module';
@@ -41,6 +42,7 @@ import { UsersModule } from '@src/apis/users/users.module';
     ClubTagsModule,
     ClubCategoriesModule,
     ClubMembersModule,
+    ClubPostsModule,
   ],
 })
 export class ApiModule {}

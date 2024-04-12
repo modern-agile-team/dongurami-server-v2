de234cd8eaf90a691ef8803278a36ab8e460a957
Lee seok ho <rrgks@naver.com>
2024-04-12 09:26:05 +0900

    최신 동아리 지원서 폼 조회 API (#174)

--- src/apis/club-application-form/services/club-application-form.service.ts
+++ src/apis/club-application-form/services/club-application-form.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { ClubApplicationFormDto } from '@src/apis/club-application-form/dto/club-application-form.dto';
 import { CreateClubApplicationFormDto } from '@src/apis/club-application-form/dto/create-club-application-form.dto';
 import { ClubApplicationFormRepository } from '@src/apis/club-application-form/repositories/club-application-form.repository';
+import { SortOrder } from '@src/constants/enum';
 
 @Injectable()
 export class ClubApplicationFormService {
@@ -22,5 +23,25 @@ export class ClubApplicationFormService {
     await this.clubApplicationFormRepository.save(newClubApplication);
 
     return new ClubApplicationFormDto(newClubApplication);
+  }
+
+  async findLatestByClubId(
+    clubId: number,
+  ): Promise<ClubApplicationFormDto | undefined> {
+    const clubApplicationForm =
+      await this.clubApplicationFormRepository.findOne({
+        where: {
+          clubId,
+        },
+        order: {
+          id: SortOrder.Desc,
+        },
+      });
+
+    if (clubApplicationForm === null) {
+      return;
+    }
+
+    return new ClubApplicationFormDto(clubApplicationForm);
   }
 }

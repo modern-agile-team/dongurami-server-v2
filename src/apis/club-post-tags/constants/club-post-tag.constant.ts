e873ed2aed455ded023949d3e6dadbe474fcc31a
hobiJeong <jjb26433@naver.com>
2024-04-16 14:03:04 +0900

    refactor/QYOG-131: club post tag name length 관련 제약 club post tag module에 작성
    https://dongurami.atlassian.net/browse/QYOG-131

---
+++ src/apis/club-post-tags/constants/club-post-tag.constant.ts
@@ -0,0 +1,4 @@
+export const CLUB_POST_TAG_NAME_LENGTH = {
+  MIN: 1,
+  MAX: 15,
+} as const;
--- src/apis/club-posts/constants/club-post.constant.ts
+++ src/apis/club-posts/constants/club-post.constant.ts
@@ -2,12 +2,7 @@ export const CLUB_POST_DESCRIPTION_LENGTH = {
   MIN: 1,
 } as const;
 
-export const CLUB_POST_HASH_TAG_ELEMENT_LENGTH = {
-  MIN: 1,
-  MAX: 15,
-} as const;
-
-export const CLUB_POST_HASH_TAG_COUNT = {
+export const CLUB_POST_TAG_COUNT = {
   MIN: 0,
   MAX: 10,
 } as const;

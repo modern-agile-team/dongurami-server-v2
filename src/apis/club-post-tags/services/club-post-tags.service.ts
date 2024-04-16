d172b1a3b736b70d6ad6a88542e49d510979ea05
hobiJeong <jjb26433@naver.com>
2024-04-16 14:05:53 +0900

    feat/QYOG-131: club post tag service 메서드 생성
    https://dongurami.atlassian.net/browse/QYOG-131

--- src/apis/club-post-tags/services/club-post-tags.service.ts
+++ src/apis/club-post-tags/services/club-post-tags.service.ts
@@ -1,8 +1,74 @@
 import { Injectable } from '@nestjs/common';
 
+import { difference } from 'lodash';
+import { In } from 'typeorm';
+
+import { ClubPostTagDto } from '@src/apis/club-post-tags/dto/club-post-tag.dto';
+import { CreateClubPostTagDto } from '@src/apis/club-post-tags/dto/create-club-post-tag.dto';
 import { ClubPostTagRepository } from '@src/apis/club-post-tags/repositories/club-post-tag.repository';
 
 @Injectable()
 export class ClubPostTagsService {
   constructor(private readonly clubPostTagRepository: ClubPostTagRepository) {}
+
+  async bulkCreate(
+    userId: number,
+    createClubPostTagDtos: CreateClubPostTagDto[],
+  ): Promise<ClubPostTagDto[]> {
+    if (!createClubPostTagDtos.length) {
+      return [];
+    }
+
+    const createClubPostTagNames = [
+      ...new Set(
+        createClubPostTagDtos.map(
+          (createClubPostTagDto) => createClubPostTagDto.name,
+        ),
+      ),
+    ];
+
+    const existClubPostTags = await this.findByNames(createClubPostTagNames);
+
+    if (existClubPostTags.length === createClubPostTagNames.length) {
+      return existClubPostTags;
+    }
+
+    const existClubPostTagNames = existClubPostTags.map(
+      (existClubPostTag) => existClubPostTag.name,
+    );
+
+    const newClubPostTagNames: string[] = difference(
+      createClubPostTagNames,
+      existClubPostTagNames,
+    );
+
+    const newClubPostTags = this.clubPostTagRepository.create(
+      newClubPostTagNames.map((name) => {
+        return {
+          userId,
+          name,
+        };
+      }),
+    );
+
+    await this.clubPostTagRepository.insert(newClubPostTags);
+
+    return newClubPostTags.map(
+      (newClubPostTag) => new ClubPostTagDto(newClubPostTag),
+    );
+  }
+
+  async findByNames(clubPostTagNames: string[]): Promise<ClubPostTagDto[]> {
+    const existClubPostTags = await this.clubPostTagRepository.findBy({
+      name: In(clubPostTagNames),
+    });
+
+    if (!existClubPostTags.length) {
+      return [];
+    }
+
+    return existClubPostTags.map(
+      (existClubPostTag) => new ClubPostTagDto(existClubPostTag),
+    );
+  }
 }

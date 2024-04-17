90a9b1d675521884dcb256d5e17facf1bb7be92d
hobiJeong <jjb26433@naver.com>
2024-04-17 16:48:50 +0900

    feat/QYOG-134: add a method to create club review to the club reviews service
    https://dongurami.atlassian.net/browse/QYOG-134

--- src/apis/club-reviews/services/club-reviews.service.ts
+++ src/apis/club-reviews/services/club-reviews.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 
+import { ClubReviewDto } from '@src/apis/club-reviews/dto/club-review.dto';
+import { CreateClubReviewDto } from '@src/apis/club-reviews/dto/create-club-review.dto';
 import { ClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository';
+import { IClubReviewRepository } from '@src/apis/club-reviews/repositories/iclub-review.repository';
 
 @Injectable()
 export class ClubReviewsService {
-  constructor(private readonly clubReviewRepository: ClubReviewRepository) {}
+  constructor(
+    @Inject(ClubReviewRepository)
+    private readonly clubReviewRepository: IClubReviewRepository,
+  ) {}
+
+  async create(
+    createClubReviewDto: CreateClubReviewDto,
+  ): Promise<ClubReviewDto> {
+    const newClubReview = await this.clubReviewRepository.createClubReview({
+      ...new CreateClubReviewDto(createClubReviewDto),
+    });
+
+    return new ClubReviewDto(newClubReview);
+  }
 }

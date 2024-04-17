6ccb338b4f537052d7ff7f5c7ce5c6dd00495596
hobiJeong <jjb26433@naver.com>
2024-04-17 16:07:42 +0900

    feat/QYOG-134: create iclub review repository
    https://dongurami.atlassian.net/browse/QYOG-134

--- src/apis/club-reviews/repositories/club-review.repository.ts
+++ src/apis/club-reviews/repositories/club-review.repository.ts
@@ -1,7 +1,10 @@
 import { Repository } from 'typeorm';
 
+import { IClubReviewRepository } from '@src/apis/club-reviews/repositories/iclub-review.repository';
 import { CustomRepository } from '@src/core/type-orm/decorators/custom-repository.decorator';
 import { ClubReview } from '@src/entities/ClubReview';
 
 @CustomRepository(ClubReview)
-export class ClubReviewRepository extends Repository<ClubReview> {}
+export class ClubReviewRepository
+  extends Repository<ClubReview>
+  implements IClubReviewRepository {}
---
+++ src/apis/club-reviews/repositories/iclub-review.repository.ts
@@ -0,0 +1,1 @@
+export interface IClubReviewRepository {}

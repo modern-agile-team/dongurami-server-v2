6b2fe2fe0ab32f631dd5031c5cbc3a468ea50f44
hobiJeong <jjb26433@naver.com>
2024-04-19 16:44:31 +0900

    refactor/QYOG-134: 주석 수정
    https://dongurami.atlassian.net/browse/QYOG-134

--- src/apis/clubs/services/clubs.service.ts
+++ src/apis/clubs/services/clubs.service.ts
@@ -609,7 +609,7 @@ export class ClubsService {
       });
     }
     /**
-     * @todo 현재 clubMember인지만을 판별하는 로직에서 동아리 지원서 승인 이력을 조회하는 로직으로 수정
+     * @todo 동아리 지원서 승인 이력을 조회하는 로직 추가
      */
     return this.clubReviewsService.create(
       new CreateClubReviewDto({

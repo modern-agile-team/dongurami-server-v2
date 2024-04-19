b095195fe5315d6bcc057e1c2f6d4a2492924381
hobiJeong <jjb26433@naver.com>
2024-04-19 16:04:36 +0900

    refactor/QYOG-134: club review repository interface 제거거
    https://dongurami.atlassian.net/browse/QYOG-134

--- src/apis/club-reviews/repositories/club-review.repository.ts
+++ src/apis/club-reviews/repositories/club-review.repository.ts
@@ -1,30 +1,7 @@
 import { Repository } from 'typeorm';
 
-import { ClubReviewStatus } from '@src/apis/club-reviews/constants/club-review.enum';
-import { CreateClubReviewDto } from '@src/apis/club-reviews/dto/create-club-review.dto';
-import { IClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository.interface';
 import { CustomRepository } from '@src/core/type-orm/decorators/custom-repository.decorator';
 import { ClubReview } from '@src/entities/ClubReview';
 
 @CustomRepository(ClubReview)
-export class ClubReviewRepository
-  extends Repository<ClubReview>
-  implements IClubReviewRepository
-{
-  createClubReview(
-    createClubReviewDto: CreateClubReviewDto,
-  ): Promise<ClubReview> {
-    return this.save(
-      this.create({
-        ...createClubReviewDto,
-        status: ClubReviewStatus.Posting,
-      }),
-    );
-  }
-
-  isExistClubReview(clubId: number, userId: number): Promise<boolean> {
-    return this.exist({
-      where: { clubId, userId, status: ClubReviewStatus.Posting },
-    });
-  }
-}
+export class ClubReviewRepository extends Repository<ClubReview> {}
--- src/apis/club-reviews/services/club-reviews.service.ts
+++ src/apis/club-reviews/services/club-reviews.service.ts
@@ -1,26 +1,23 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 import { ClubReviewDto } from '@src/apis/club-reviews/dto/club-review.dto';
 import { CreateClubReviewDto } from '@src/apis/club-reviews/dto/create-club-review.dto';
 import { ClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository';
-import { IClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository.interface';
 import { CLUB_REVIEW_ERROR_CODE } from '@src/constants/error/club-review/club-review-error-code.constant';
 import { HttpConflictException } from '@src/http-exceptions/exceptions/http-conflict.exception';
 
 @Injectable()
 export class ClubReviewsService {
-  constructor(
-    @Inject(ClubReviewRepository)
-    private readonly clubReviewRepository: IClubReviewRepository,
-  ) {}
+  constructor(private readonly clubReviewRepository: ClubReviewRepository) {}
 
   async create(
     createClubReviewDto: CreateClubReviewDto,
   ): Promise<ClubReviewDto> {
-    const isExistClubReview = await this.clubReviewRepository.isExistClubReview(
-      createClubReviewDto.clubId,
-      createClubReviewDto.userId,
-    );
+    const { clubId, userId } = createClubReviewDto;
+
+    const isExistClubReview = await this.clubReviewRepository.exist({
+      where: { clubId, userId },
+    });
 
     if (isExistClubReview) {
       throw new HttpConflictException({
@@ -28,9 +25,11 @@ export class ClubReviewsService {
       });
     }
 
-    const newClubReview = await this.clubReviewRepository.createClubReview(
+    const newClubReview = this.clubReviewRepository.create(
       new CreateClubReviewDto(createClubReviewDto),
     );
+
+    await this.clubReviewRepository.save(newClubReview);
 
     return new ClubReviewDto(newClubReview);
   }

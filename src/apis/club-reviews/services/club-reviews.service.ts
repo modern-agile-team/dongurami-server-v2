33471ebd9c6f54cb1fe901f85966c8141bbcecc1
hobiJeong <jjb26433@naver.com>
2024-04-19 14:55:33 +0900

    refactor/QYOG-134: spread 연산자 빼기
    https://dongurami.atlassian.net/browse/QYOG-134

--- src/apis/club-reviews/services/club-reviews.service.ts
+++ src/apis/club-reviews/services/club-reviews.service.ts
@@ -3,7 +3,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClubReviewDto } from '@src/apis/club-reviews/dto/club-review.dto';
 import { CreateClubReviewDto } from '@src/apis/club-reviews/dto/create-club-review.dto';
 import { ClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository';
-import { IClubReviewRepository } from '@src/apis/club-reviews/repositories/iclub-review.repository';
+import { IClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository.interface';
 import { CLUB_REVIEW_ERROR_CODE } from '@src/constants/error/club-review/club-review-error-code.constant';
 import { HttpConflictException } from '@src/http-exceptions/exceptions/http-conflict.exception';
 
@@ -28,9 +28,9 @@ export class ClubReviewsService {
       });
     }
 
-    const newClubReview = await this.clubReviewRepository.createClubReview({
-      ...new CreateClubReviewDto(createClubReviewDto),
-    });
+    const newClubReview = await this.clubReviewRepository.createClubReview(
+      new CreateClubReviewDto(createClubReviewDto),
+    );
 
     return new ClubReviewDto(newClubReview);
   }

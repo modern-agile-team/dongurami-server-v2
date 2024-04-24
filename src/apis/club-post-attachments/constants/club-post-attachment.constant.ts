9aa64e61a22dd52e8b6e7db6fc4b7fcb010cc8e9
hobiJeong <jjb26433@naver.com>
2024-04-24 20:41:28 +0900

    feat/QYOG-137: ClubPostAttachments 모듈 구성 및 제약조건 추가
    https://dongurami.atlassian.net/browse/QYOG-137

---
+++ src/apis/club-post-attachments/constants/club-post-attachment.constant.ts
@@ -0,0 +1,9 @@
+export const CLUB_POST_ATTACHMENT_PATH_LENGTH = {
+  MIN: 19,
+  MAX: 19,
+} as const;
+
+export const CLUB_POST_ATTACHMENT_COUNT = {
+  MIN: 0,
+  MAX: 10,
+} as const;
--- src/apis/clubs/dto/create-club-post-request-body.dto.ts
+++ src/apis/clubs/dto/create-club-post-request-body.dto.ts
@@ -3,6 +3,10 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { ArrayMaxSize, Length, MinLength } from 'class-validator';
 
 import {
+  CLUB_POST_ATTACHMENT_COUNT,
+  CLUB_POST_ATTACHMENT_PATH_LENGTH,
+} from '@src/apis/club-post-attachments/constants/club-post-attachment.constant';
+import {
   CLUB_POST_DESCRIPTION_LENGTH,
   CLUB_POST_TAG_COUNT,
 } from '@src/apis/club-posts/constants/club-post.constant';
@@ -32,4 +36,13 @@ export class CreateClubPostRequestBodyDto
     each: true,
   })
   tagNames: string[] = [];
+
+  @ApiPropertyOptional({
+    description: '동아리 게시글 첨부 파일',
+    minLength: CLUB_POST_ATTACHMENT_PATH_LENGTH.MIN,
+    maxLength: CLUB_POST_ATTACHMENT_PATH_LENGTH.MAX,
+    minItems: CLUB_POST_ATTACHMENT_COUNT.MIN,
+    maxItems: CLUB_POST_ATTACHMENT_COUNT.MAX,
+  })
+  paths: string[] = [];
 }

29aa02e90914552630dcbcb82431e0e31d8dd863
hobiJeong <jjb26433@naver.com>
2024-04-24 18:26:46 +0900

    db/QYOG-137: club_post_attachment migration 파일 수정
    https://dongurami.atlassian.net/browse/QYOG-137

--- migrations/club/1713943114954-club-post-attachment.ts
+++ migrations/club/1713943114954-club-post-attachment.ts
@@ -1,7 +1,6 @@
 import {
   generateCreatedAtColumn,
   generateFkColumn,
-  generatePrimaryColumn,
 } from 'migrations/__utils/util';
 import { MigrationInterface, QueryRunner, Table, TableColumn } from 'typeorm';
 
@@ -17,25 +16,20 @@ export class ClubPostAttachment1713943114954 implements MigrationInterface {
         name: 'club_post_attachment',
         columns: [
           new TableColumn({
-            name: 'attachment_id',
-            type: 'bigint',
-            unsigned: true,
+            /**
+             * @todo 현재 attachment 테이블의 path 컬럼은 index가 존재하지 않아서 외래 키를 생성하지 못함.
+             * 추후 url 혹은 path 중 어떤 것에 index를 생성할 것인지에 대해서 정해지면 그에 맞게 컬럼명 수정 및 외래키 생성
+             */
+            name: 'attachment_path',
+            type: 'varchar',
+            length: '18',
             isNullable: false,
-            comment: '첨부 파일 고유 ID',
+            comment: 'domain을 제외한 path',
           }),
           clubPostColumn,
           generateCreatedAtColumn(),
         ],
-        foreignKeys: [
-          {
-            columnNames: ['attachment_id'],
-            referencedTableName: 'attachment',
-            referencedColumnNames: ['id'],
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE',
-          },
-          clubPostFk,
-        ],
+        foreignKeys: [clubPostFk],
       }),
     );
 

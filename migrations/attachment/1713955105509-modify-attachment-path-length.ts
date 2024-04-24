95acbf6379894b4a25d006f64a55ad70fe91d382
hobiJeong <jjb26433@naver.com>
2024-04-24 20:45:11 +0900

    db/QYOG-142: modify-attachment-path-length migration 파일 수정
    https://dongurami.atlassian.net/browse/QYOG-142

--- migrations/attachment/1713955105509-modify-attachment-path-length.ts
+++ migrations/attachment/1713955105509-modify-attachment-path-length.ts
@@ -4,10 +4,14 @@ export class ModifyAttachmentPathLength1713955105509
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE attachment MODIFY path varchar(19)');
+    await queryRunner.query(
+      'ALTER TABLE attachment MODIFY path VARCHAR(19) NOT NULL COMMENT "domain을 제외한 path"',
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE attachment MODIFY path varchar(18)');
+    await queryRunner.query(
+      'ALTER TABLE attachment MODIFY path VARCHAR(18) NOT NULL COMMENT "domain을 제외한 path"',
+    );
   }
 }

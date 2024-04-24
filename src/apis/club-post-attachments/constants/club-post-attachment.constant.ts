8adbc79690386a889e2f75432e5af57dacf1b994
hobiJeong <jjb26433@naver.com>
2024-04-25 02:19:54 +0900

    feat/QYOG-137: club post attachment mime type array 추가
    https://dongurami.atlassian.net/browse/QYOG-137

--- src/apis/club-post-attachments/constants/club-post-attachment.constant.ts
+++ src/apis/club-post-attachments/constants/club-post-attachment.constant.ts
@@ -1,5 +1,5 @@
 export const CLUB_POST_ATTACHMENT_PATH_LENGTH = {
-  MIN: 19,
+  MIN: 1,
   MAX: 19,
 } as const;
 
@@ -7,3 +7,12 @@ export const CLUB_POST_ATTACHMENT_COUNT = {
   MIN: 0,
   MAX: 10,
 } as const;
+
+export const CLUB_POST_ATTACHMENT_MIME_TYPE = [
+  'image/png',
+  'image/jpg',
+  'image/jpeg',
+  'video/mp4',
+  'video/mov',
+  'video/mpeg4',
+];

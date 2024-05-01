7dcfab1d9a6d0c12431d156b6dc1449e7c5e346d
hobiJeong <jjb26433@naver.com>
2024-05-02 05:41:24 +0900

    feat/QYOG-146: reactionType property 추가가
    https://dongurami.atlassian.net/browse/QYOG-146

--- src/apis/free-posts/dto/free-post-reaction.dto.ts
+++ src/apis/free-posts/dto/free-post-reaction.dto.ts
@@ -5,19 +5,21 @@ import { Exclude, Expose } from 'class-transformer';
 import { ReactionTypes } from '@src/apis/reactions/constants/reaction.enum';
 import { BaseDto } from '@src/dto/base.dto';
 import { FreePostReaction } from '@src/entities/FreePostReaction';
+import { ReactionType } from '@src/entities/ReactionType';
 
 export class FreePostReactionDto
   extends PickType(BaseDto, ['id', 'createdAt'] as const)
-  implements Pick<FreePostReaction, 'id' | 'userId' | 'parentId' | 'createdAt'>
+  implements
+    Pick<
+      FreePostReaction,
+      'id' | 'userId' | 'parentId' | 'createdAt' | 'reactionType'
+    >
 {
   @ApiProperty({
     description: 'reaction 등록 유저 고유 ID',
     format: 'integer',
   })
   userId: number;
-
-  @Exclude({ toPlainOnly: true })
-  parentId: number;
 
   @ApiProperty({
     description: '자유 게시글 고유 ID',
@@ -32,7 +34,16 @@ export class FreePostReactionDto
   @ApiProperty({
     description: 'reaction type',
     enum: ReactionTypes,
+    type: 'string',
   })
   @Expose({ toPlainOnly: true })
-  type: string;
+  get type() {
+    return this.reactionType.name;
+  }
+
+  @Exclude({ toPlainOnly: true })
+  parentId: number;
+
+  @Exclude({ toPlainOnly: true })
+  reactionType: ReactionType;
 }

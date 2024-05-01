bb5af1b26eca6c2ce41b5c2e90210ae1ed487a38
hobiJeong <jjb26433@naver.com>
2024-05-02 05:25:12 +0900

    feat/QYOG-146: const 및 free post reaction 관련 dto 생성
    https://dongurami.atlassian.net/browse/QYOG-146

---
+++ src/apis/free-posts/dto/free-post-reaction.dto.ts
@@ -0,0 +1,38 @@
+import { ApiProperty, PickType } from '@nestjs/swagger';
+
+import { Exclude, Expose } from 'class-transformer';
+
+import { ReactionType } from '@src/apis/reactions/constants/reaction.enum';
+import { BaseDto } from '@src/dto/base.dto';
+import { FreePostReaction } from '@src/entities/FreePostReaction';
+
+export class FreePostReactionDto
+  extends PickType(BaseDto, ['id', 'createdAt'] as const)
+  implements Pick<FreePostReaction, 'id' | 'userId' | 'parentId' | 'createdAt'>
+{
+  @ApiProperty({
+    description: 'reaction 등록 유저 고유 ID',
+    format: 'integer',
+  })
+  userId: number;
+
+  @Exclude({ toPlainOnly: true })
+  parentId: number;
+
+  @ApiProperty({
+    description: '자유 게시글 고유 ID',
+    format: 'integer',
+    type: 'number',
+  })
+  @Expose({ toPlainOnly: true })
+  get freePostId() {
+    return this.parentId;
+  }
+
+  @ApiProperty({
+    description: 'reaction type',
+    enum: ReactionType,
+  })
+  @Expose({ toPlainOnly: true })
+  type: string;
+}

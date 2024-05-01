c9187648e9cc823771e2bf45b1e8c0ed3f1671f1
hobiJeong <jjb26433@naver.com>
2024-05-02 05:18:27 +0900

    feat/QYOG-146: RequiredReactionColumn에 reactionType 추가
    https://dongurami.atlassian.net/browse/QYOG-146

--- src/apis/reactions/types/reaction.type.ts
+++ src/apis/reactions/types/reaction.type.ts
@@ -1,5 +1,8 @@
+import { ReactionType } from '@src/entities/ReactionType';
+
 export interface RequiredReactionColumn {
   reactionTypeId: number;
   parentId: number;
   userId: number;
+  reactionType: ReactionType;
 }

28978e00c7fdadac724418c3f5b24db981db74e6
hobiJeong <jjb26433@naver.com>
2024-05-16 23:12:59 +0900

    feat/QYOG-147: club review order field 추가
    https://dongurami.atlassian.net/browse/QYOG-147

--- src/apis/club-reviews/constants/club-review.constant.ts
+++ src/apis/club-reviews/constants/club-review.constant.ts
@@ -1,3 +1,5 @@
+import { ClubReviewDto } from '@src/apis/club-reviews/dto/club-review.dto';
+
 export const CLUB_REVIEW_DESCRIPTION_LENGTH = {
   MIN: 1,
   MAX: 255,
@@ -7,3 +9,13 @@ export const CLUB_REVIEW_STAR_RATE_RANGE = {
   MIN: 1,
   MAX: 5,
 } as const;
+
+export const CLUB_REVIEW_ORDER_FIELD: readonly (keyof ClubReviewDto)[] = [
+  'id',
+  'userId',
+  'description',
+  'starRate',
+  'isAnonymous',
+  'createdAt',
+  'updatedAt',
+] as const;

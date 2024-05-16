6d4a04846a9ec9602ab5fc146590a2e9b7f8aa04
hobiJeong <jjb26433@naver.com>
2024-05-16 23:09:28 +0900

    feat/QYOG-147: QueryHelper provider 등록
    https://dongurami.atlassian.net/browse/QYOG-147

--- src/apis/club-reviews/club-reviews.module.ts
+++ src/apis/club-reviews/club-reviews.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { ClubReviewRepository } from '@src/apis/club-reviews/repositories/club-review.repository';
 import { ClubReviewsService } from '@src/apis/club-reviews/services/club-reviews.service';
 import { TypeOrmExModule } from '@src/core/type-orm/type-orm-ex.module';
+import { QueryHelper } from '@src/helpers/query.helper';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([ClubReviewRepository])],
-  providers: [ClubReviewsService],
+  providers: [ClubReviewsService, QueryHelper],
   exports: [TypeOrmExModule, ClubReviewsService],
 })
 export class ClubReviewsModule {}

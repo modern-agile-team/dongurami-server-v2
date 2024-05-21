d31811b94ad96b2178f06ffdf0749700fe1fefd3
Lee seok ho <rrgks@naver.com>
2024-05-21 09:10:37 +0900

    공지게시글 단일 응답에 user prop 추가 (#199)

--- src/apis/notice-posts/services/notice-posts.service.ts
+++ src/apis/notice-posts/services/notice-posts.service.ts
@@ -19,6 +19,7 @@ import { PostTagsService } from '@src/apis/post-tags/services/post-tags.service'
 import { CreateReactionDto } from '@src/apis/reactions/dto/create-reaction.dto';
 import { RemoveReactionDto } from '@src/apis/reactions/dto/remove-reaction.dto';
 import { ReactionsService } from '@src/apis/reactions/services/reactions.service';
+import { UsersService } from '@src/apis/users/services/users.service';
 import { COMMON_ERROR_CODE } from '@src/constants/error/common/common-error-code.constant';
 import { NoticePost } from '@src/entities/NoticePost';
 import { NoticePostReaction } from '@src/entities/NoticePostReaction';
@@ -38,6 +39,7 @@ export class NoticePostsService {
     private readonly reactionsService: ReactionsService<NoticePostReaction>,
     private readonly commonPostsService: CommonPostsService<NoticePost>,
     private readonly postTagsService: PostTagsService,
+    private readonly usersService: UsersService,
 
     private readonly noticePostRepository: NoticePostRepository,
     private readonly noticePostTagLinkRepository: NoticePostTagLinkRepository,
@@ -62,9 +64,11 @@ export class NoticePostsService {
       tags: postTags,
     });
 
+    const postingUser = await this.usersService.findOneById(userId);
+
     await this.bulkAppendTagLink(userId, newPost.id, postTags);
 
-    return new NoticePostDto({ ...newPost, postTags });
+    return new NoticePostDto({ ...newPost, postTags, user: postingUser });
   }
 
   async findAllAndCount(
@@ -98,9 +102,14 @@ export class NoticePostsService {
   }
 
   async findOneOrNotFound(noticePostId: number): Promise<NoticePostDto> {
-    const noticePost = await this.noticePostRepository.findOneBy({
-      id: noticePostId,
-      status: NoticePostStatus.Posting,
+    const noticePost = await this.noticePostRepository.findOne({
+      where: {
+        id: noticePostId,
+        status: NoticePostStatus.Posting,
+      },
+      relations: {
+        user: true,
+      },
     });
 
     if (!noticePost) {
@@ -229,7 +238,13 @@ export class NoticePostsService {
 
   @Transactional()
   async remove(userId: number, noticePostId: number): Promise<number> {
-    const existPost = await this.findOneOrNotFound(noticePostId);
+    const existPost = await this.findOne(noticePostId);
+
+    if (!existPost) {
+      throw new HttpNotFoundException({
+        code: COMMON_ERROR_CODE.RESOURCE_NOT_FOUND,
+      });
+    }
 
     if (existPost.userId !== userId) {
       throw new HttpForbiddenException({
